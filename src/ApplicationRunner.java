70ec5c7614ea59e808582e94d04f3756cdcba403
= <av3977@g.rit.edu>
2022-10-23 15:47:23 -0400

    demo: best, average and worst cases

--- src/ApplicationRunner.java
+++ src/ApplicationRunner.java
@@ -32,16 +32,17 @@ public class ApplicationRunner {
                 int randomNumber = ThreadLocalRandom.current().nextInt(1, 9 + 1);
                 consumers[i] = new Consumer("Consumer-" + String.valueOf(i), resource, randomNumber, System.currentTimeMillis());
                 consumersThreads[i] = new Thread(consumers[i]);
-                consumersThreads[i].setPriority(randomNumber);
+                consumersThreads[i].setPriority(randomNumber); // Priority scheduling.
+//                consumersThreads[i].setPriority(10 - randomNumber); --> SJF [Shortest job first]
             }
-            int initialConsumerProcess = 7;
+            int initialConsumerProcess = 3;
 
-            System.out.println("------------- -------------------------------------------------------------------");
+            System.out.println("--------------------------------------------------------------------------------");
             System.out.println("--------Executing first " +initialConsumerProcess+ " consumers--------");
             for (int i = 0; i<initialConsumerProcess; i++) {
                 poolExecutor.execute(consumersThreads[i]);
             }
-
+            // seen 8 threads until here
             Timer timer = new Timer(3000, new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent arg0) {
@@ -75,8 +76,23 @@ public class ApplicationRunner {
             System.out.println("--------Sleeping main thread for 20 seconds--------");
             Thread.sleep(20000);
             System.out.println("--------Scheduling next 5 consumers--------");
-            for (int i = 0; i<5; i++)
+
+            // Core pool: 10
+            // Max pool: 15
+            // room outside the store: 5 -- since they are not in the store, there priority doesn't matter.
+
+            // 1 + 7 = 8 (initially)
+            // wait for 20 seconds
+            // 10 more consumers to the store
+
+            // 10-8 = 2
+
+            // room: 2 + 5 = 7
+            // 10 - 7 = -3 (java.util.concurrent.RejectedExecutionException)
+
+            for (int i = 0; i<3; i++)
                 poolExecutor.execute(consumersThreads[i+initialConsumerProcess]);
+
             poolExecutor.shutdown();
         }
         catch (Exception e) {
